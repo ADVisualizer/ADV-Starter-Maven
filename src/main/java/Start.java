c3a0796a6d365fd5f188f1161fe61e80606a99b3
Murièle Trentini <muriele@trentini.ch>
2018-05-31 11:02:40 +0200

    initial commit

---
+++ src/main/java/Start.java
@@ -0,0 +1,11 @@
+import ch.hsr.adv.commons.core.logic.util.ADVException;
+import ch.hsr.adv.lib.bootstrapper.ADV;
+
+public class Start {
+    public static void main(String[] args) throws ADVException {
+
+        ADV.launch(args);
+
+
+    }
+}
